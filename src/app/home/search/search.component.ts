9d5152415aeca4c333d786b73cb286d0852f6634
Javier S. Panchi <javier.s.panchi@au.abb.com>
2017-12-22 14:43:18 +1000

    Changes to use table

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -4,6 +4,8 @@ import {MatSnackBar} from '@angular/material';
 import {CommonDialogComponent} from '../../dialogs/common-dialog/common-dialog.component';
 import {MatDialogRef,MatDialog} from '@angular/material';
 import {LocalstorageService} from '../../services/localstorage.service';
+import {MatPaginator,MatTableDataSource} from '@angular/material';
+import {ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -12,6 +14,15 @@ import {LocalstorageService} from '../../services/localstorage.service';
   providers:[GithubService,LocalstorageService]
 })
 export class SearchComponent implements OnInit {
+
+  displayedColumns = ['name', 'date'];
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   username:String;
   repoContents:Array<Object>=[];
@@ -40,7 +51,8 @@ export class SearchComponent implements OnInit {
   showContents(){
     this.repoContents=[];
     this.myService.getRepositories(this.username).subscribe(posts=>{
-      this.repoContents=posts;
+      this.dataSource=new MatTableDataSource<Element>(posts);
+      this.dataSource.paginator = this.paginator;
       this.showSearchBox=false;
     }, err =>{
       this.showSnackBar("Not Found", "Connection Error");
@@ -66,3 +78,32 @@ export class SearchComponent implements OnInit {
   }
 
 }
+
+export interface Element {
+  name: string;
+  pushed_at: Date;
+}
+
+
+const ELEMENT_DATA: Element[] = [
+  {name: 'Hydrogen', pushed_at:new Date("12/12/2004")},
+  {name: 'Helium', pushed_at:new Date("12/12/2004")},
+  {name: 'Lithium', pushed_at:new Date("12/12/2004")},
+  {name: 'Beryllium', pushed_at:new Date("12/12/2004")},
+  {name: 'Boron', pushed_at:new Date("12/12/2004")},
+  {name: 'Carbon', pushed_at:new Date("12/12/2004")},
+  {name: 'Nitrogen', pushed_at:new Date("12/12/2004")},
+  {name: 'Oxygen', pushed_at:new Date("12/12/2004")},
+  {name: 'Fluorine', pushed_at:new Date("12/12/2004")},
+  {name: 'Neon', pushed_at:new Date("12/12/2004")},
+  {name: 'Sodium', pushed_at:new Date("12/12/2004")},
+  {name: 'Magnesium', pushed_at:new Date("12/12/2004")},
+  {name: 'Aluminum', pushed_at:new Date("12/12/2004")},
+  {name: 'Silicon', pushed_at:new Date("12/12/2004")},
+  {name: 'Phosphorus', pushed_at:new Date("12/12/2004")},
+  {name: 'Sulfur', pushed_at:new Date("12/12/2004")},
+  {name: 'Chlorine', pushed_at:new Date("12/12/2004")},
+  {name: 'Argon', pushed_at:new Date("12/12/2004")},
+  {name: 'Potassium', pushed_at:new Date("12/12/2004")},
+  {name: 'Calcium', pushed_at:new Date("12/12/2004")}
+]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule,MatInputModule,MatIconModule, MatCardModule,MatSnackBarModule,MatListModule,MatTabsModule,MatDialogModule,MatSelectModule,MatIconModule  } from '@angular/material';
+import { MatButtonModule, MatToolbarModule,MatInputModule,MatIconModule, MatCardModule,MatSnackBarModule,MatListModule,MatTabsModule,MatDialogModule,MatSelectModule,MatTableModule,MatPaginatorModule  } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatInputModule,MatIconModule, MatCardModule,MatSnackBarModule,MatListModule,MatTabsModule,MatDialogModule,MatSelectModule,MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatInputModule,MatIconModule, MatCardModule,MatSnackBarModule,MatListModule,MatTabsModule,MatDialogModule,MatSelectModule,MatIconModule],
+  imports: [MatButtonModule, MatToolbarModule, MatInputModule,MatIconModule, MatCardModule,MatSnackBarModule,MatListModule,MatTabsModule,MatDialogModule,MatSelectModule,MatTableModule,MatPaginatorModule],
+  exports: [MatButtonModule, MatToolbarModule, MatInputModule,MatIconModule, MatCardModule,MatSnackBarModule,MatListModule,MatTabsModule,MatDialogModule,MatSelectModule,MatTableModule,MatPaginatorModule],
 })
 export class MaterialModule { }
